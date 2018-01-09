e5e46d37b8bae3d636c42c0ccbb6e75c5aa26e4c
Leonardo Cardoso <leocardosoti@gmail.com>
2018-01-09 10:10:30 -0200

    Smoke tests + Sinon

---
+++ src/main.js
@@ -0,0 +1,5 @@
+export const search = () => fetch('https://developer.spotify.com/web-api/');
+export const searchArtist = () => { };
+export const searchAlbumns = () => { };
+export const searchPlaylist = () => { };
+export const searchTracks = () => { };
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,47 @@
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import sinonStubPromise from 'sinon-stub-promise';
 
-describe('Main', () => {
+import { search, searchAlbumns, searchArtist, searchPlaylist, searchTracks } from './../src/main';
 
+chai.use(sinonChai);
+sinonStubPromise(sinon);
+
+global.fetch = require('node-fetch');
+
+describe('Spotify Wrapper', () => {
+
+  describe('smoke tests', () => {
+    it('should exist the search method', () => {
+      expect(search).to.exist;
+    });
+
+    it('should exist the searchAlbumns method', () => {
+      expect(searchAlbumns).to.exist;
+    });
+
+    it('should exist the searchArtist method', () => {
+      expect(searchArtist).to.exist;
+    });
+
+    it('should exist the searchTracks method', () => {
+      expect(searchTracks).to.exist;
+    });
+
+    it('should exist the searchPlaylist method', () => {
+      expect(searchPlaylist).to.exist;
+    });
+
+  });
+
+  describe('Generic Search', () => {
+    it('should call fetch function', () => {
+      const fetchedStub = sinon.stub(global, 'fetch');
+
+      const artists = search();
+
+      expect(fetchedStub).to.have.been.calledOnce;
+    });
+  });
 });
